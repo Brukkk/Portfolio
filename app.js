490e2f12308b102fad9d29712d7cf8cb3ae3a4c1
Brukkk <bruktorra@gmail.com>
2021-10-08 19:20:36 -0300

    alaining welcome and navbar

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const menuIcon = document.querySelector('#mobile-menu'); /* we select the hamburger menu + the rotations */
 const menuLinks = document.querySelector('.navbar__menu'); /* select our UL to display from top:-1000px */
+const menuItems = document.getElementsByClassName("navbar__links");
 
 // Display Mobile Menu
 
-const mobileMenu = () => {
+const openMobileMenu = () => {
     menuIcon.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+
+    
 }
 
-menuIcon.addEventListener('click',mobileMenu);
+menuIcon.addEventListener('click',openMobileMenu);
+
+
