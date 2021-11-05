16d4de75574c0f0c10c176fc1862d063c2d48ec8
Brukkk <bruktorra@gmail.com>
2021-11-05 10:20:35 -0300

    Update variable name for UL mobile nav menu
    improve code readability

--- app.js
+++ app.js
@@ -22,9 +22,9 @@
       
       // Remove menu mobile
       const navMenu = document.querySelector('.navbar__menu');
-      const nav = document.getElementById("mobile-menu");
-      if (nav.classList.contains("is-active")){
-        nav.classList.toggle("is-active");
+      const mobileNav = document.getElementById("mobile-menu");
+      if (mobileNav.classList.contains("is-active")){
+        mobileNav.classList.toggle("is-active");
       }
       navMenu.classList.remove('active');
       
